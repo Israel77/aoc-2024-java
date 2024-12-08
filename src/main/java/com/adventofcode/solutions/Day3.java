daf7fb875addaccb92b7509f333c9ee3dd2e818f
Israel Batista <israel.silvino@gmail.com>
2024-12-08 14:21:19 -0300

    feat: Add solution for day 3 part 2

--- src/main/java/com/adventofcode/solutions/Day3.java
+++ src/main/java/com/adventofcode/solutions/Day3.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public enum Day3 implements Solver<Integer> {
     INSTANCE;
@@ -11,32 +12,81 @@ public enum Day3 implements Solver<Integer> {
     @Override
     public Integer solvePart1(String input) {
         return parseInput(input).stream()
+                .filter(i -> i instanceof Mul)
+                .map(i -> (Mul) i)
                 .mapToInt(pair -> pair.first() * pair.second())
                 .sum();
     }
 
     @Override
     public Integer solvePart2(String input) {
-        // TODO Auto-generated method stub
-        return Solver.super.solvePart2(input);
-    }
-
-    List<Mul> parseInput(String input) {
-        List<Mul> result = new ArrayList<>();
-
-        Pattern mul = Pattern.compile("mul\\((\\d+),(\\d+)\\)");
-        Matcher mulMatcher = mul.matcher(input);
-
-        while (mulMatcher.find()) {
-            int first = Integer.parseInt(mulMatcher.group(1));
-            int second = Integer.parseInt(mulMatcher.group(2));
-
-            result.add(new Mul(first, second));
+        int result = 0;
+        boolean enable = true;
+        for (Instruction instruction : parseInput(input)) {
+            switch (instruction) {
+                case Mul m:
+                    result += enable ? m.first() * m.second() : 0;
+                    break;
+                case Do __:
+                    enable = true;
+                    break;
+                case Dont __:
+                    enable = false;
+                    break;
+            }
         }
-
         return result;
     }
 
-    private record Mul(Integer first, Integer second) {
+    static List<Instruction> parseInput(String input) {
+        String sanitizedInput = input.trim().lines()
+                .map(line -> line.trim())
+                .collect(Collectors.joining());
+        List<Instruction> result = new ArrayList<>();
+
+        Pattern mul = Pattern.compile("^(mul\\((\\d+),(\\d+)\\)).*?");
+        Pattern _do = Pattern.compile("^do().*?");
+        Pattern dont = Pattern.compile("^don\'t().*?");
+
+        int currentIndex = 0;
+        while (currentIndex < sanitizedInput.length()) {
+            var currentSubstring = sanitizedInput.substring(currentIndex);
+
+            Matcher mulMatcher = mul.matcher(currentSubstring);
+            Matcher doMatcher = _do.matcher(currentSubstring);
+            Matcher dontMatcher = dont.matcher(currentSubstring);
+
+            if (dontMatcher.matches()) {
+                result.add(new Dont());
+                currentIndex += 6;
+            } else if (doMatcher.matches()) {
+                result.add(new Do());
+                currentIndex += 4;
+            } else if (mulMatcher.matches()) {
+                int first = Integer.parseInt(mulMatcher.group(2));
+                int second = Integer.parseInt(mulMatcher.group(3));
+
+                result.add(new Mul(first, second));
+                currentIndex += mulMatcher.group(1).length();
+            } else {
+                currentIndex++;
+            }
+        }
+
+        System.out.println("Parsed " + result.size() + " expressions");
+        System.out.println(result);
+        return result;
+    }
+
+    private sealed interface Instruction permits Mul, Do, Dont {
+    }
+
+    private record Mul(Integer first, Integer second) implements Instruction {
+    }
+
+    private record Do() implements Instruction {
+    }
+
+    private record Dont() implements Instruction {
     }
 }
--- src/test/java/com/adventofcode/solutions/Day3Test.java
+++ src/test/java/com/adventofcode/solutions/Day3Test.java
@@ -7,18 +7,20 @@ import org.junit.jupiter.api.Test;
 
 public class Day3Test {
     Solver<Integer> solver = Day3.INSTANCE;
-    String input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
 
     @Test
     @DisplayName("Should solve the example given in part 1")
     void testSolverPart1() {
+        String input = """
+                (%%xwhen()mul(2,4))%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))""";
         assertEquals(161, solver.solvePart1(input));
     }
 
     @Test
     @DisplayName("Should solve the example given in part 2")
     void testSolverPart2() {
-        // assertEquals(4, solver.solvePart2(input));
+        String input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+        assertEquals(48, solver.solvePart2(input));
     }
 
 }
