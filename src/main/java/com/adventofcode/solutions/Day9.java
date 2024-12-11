4563827281428fad4a50eea1517f335cfb642acb
Israel Batista <israel.silvino@gmail.com>
2024-12-11 13:41:07 -0300

    refactor: Change naming to match problema description of Day 9

--- src/main/java/com/adventofcode/solutions/Day9.java
+++ src/main/java/com/adventofcode/solutions/Day9.java
@@ -10,26 +10,26 @@ public enum Day9 implements Solver<Long, Integer> {
     @Override
     public Long solvePart1(String input) {
         Disk disk = parseInput(input);
-        List<File> files = disk.files();
+        List<FileBlock> fileBlocks = disk.fileBlocks();
         List<Integer> freeSpace = disk.freeSpace();
 
-        List<File> defragmentedFiles = new ArrayList<>();
+        List<FileBlock> compressedFileBlocks = new ArrayList<>();
         // Reverse sort by position
-        files.sort((a, b) -> Integer.compare(b.pos, a.pos));
+        fileBlocks.sort((a, b) -> Integer.compare(b.pos, a.pos));
         boolean addRemaining = false;
-        for (var file : files) {
-            addRemaining = addRemaining || freeSpace.isEmpty() || freeSpace.get(0) >= files.size();
+        for (var fileBlock : fileBlocks) {
+            addRemaining = addRemaining || freeSpace.isEmpty() || freeSpace.get(0) >= fileBlocks.size();
             if (addRemaining) {
-                defragmentedFiles.add(file);
+                compressedFileBlocks.add(fileBlock);
             } else {
                 int newPos = freeSpace.removeFirst();
-                File newFile = file.withPos(newPos);
-                defragmentedFiles.add(newFile);
+                FileBlock newFile = fileBlock.withPos(newPos);
+                compressedFileBlocks.add(newFile);
             }
         }
 
-        return defragmentedFiles.stream()
-                .mapToLong(File::getFileChecksum)
+        return compressedFileBlocks.stream()
+                .mapToLong(FileBlock::getFileChecksum)
                 .sum();
     }
 
@@ -40,7 +40,7 @@ public enum Day9 implements Solver<Long, Integer> {
     }
 
     Disk parseInput(String input) {
-        List<File> files = new ArrayList<>();
+        List<FileBlock> files = new ArrayList<>();
         List<Integer> freeSpace = new ArrayList<>();
 
         int fileCount = 0;
@@ -52,7 +52,7 @@ public enum Day9 implements Solver<Long, Integer> {
             var amount = ch - 48;
 
             if (i % 2 == 0) {
-                List<File> fileRange = generateFileRange(fileId, totalCount, amount);
+                List<FileBlock> fileRange = generateFileBlockRange(fileId, totalCount, amount);
                 files.addAll(fileRange);
                 fileCount += amount;
                 fileId++;
@@ -70,31 +70,102 @@ public enum Day9 implements Solver<Long, Integer> {
         return new Disk(files, freeSpace, totalCount);
     }
 
-    private static List<File> generateFileRange(int id, int startPosition, int amount) {
-        List<File> result = new ArrayList<>();
+    private static List<FileBlock> generateFileBlockRange(int id, int startPosition, int amount) {
+        List<FileBlock> result = new ArrayList<>();
 
         for (int i = 0; i < amount; ++i) {
             int pos = startPosition + i;
-            result.add(new File(id, pos));
+            result.add(new FileBlock(id, pos));
         }
 
         return result;
     }
 
-    private record Disk(List<File> files, List<Integer> freeSpace, int size) {
+    private record Disk(List<FileBlock> fileBlocks, List<Integer> freeSpace, int size) {
     }
 
-    private static class File {
+    private static class MutableRange {
+        private int _start;
+        private int _end;
+
+        public MutableRange(int start, int end) {
+            if (_start > _end)
+                throw new IllegalArgumentException("Range end cannot be smaller than start");
+            this._start = start;
+            this._end = end;
+        }
+
+        public int size() {
+            return _start - _end + 1;
+        }
+
+        public int start() {
+            return this._start;
+        }
+
+        public int end() {
+            return this._end;
+        }
+
+        public void removeFromStart(int amount) {
+            if (amount > size()) {
+                throw new IllegalArgumentException(
+                        String.format("Cannot remove %s from range with size %s", amount, size()));
+            }
+            this._start += amount;
+        }
+
+        public void removeFromEnd(int amount) {
+            if (amount > size()) {
+                throw new IllegalArgumentException(
+                        String.format("Cannot remove %s from range with size %s", amount, size()));
+            }
+            this._end -= amount;
+
+        }
+
+        @Override
+        public String toString() {
+            return "Range [start=" + _start + ", end=" + _end + "]";
+        }
+
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + _start;
+            result = prime * result + _end;
+            return result;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            MutableRange other = (MutableRange) obj;
+            if (_start != other._start)
+                return false;
+            if (_end != other._end)
+                return false;
+            return true;
+        }
+    }
+
+    private static class FileBlock {
         public int id;
         public int pos;
 
-        public File(int id, int pos) {
+        public FileBlock(int id, int pos) {
             this.id = id;
             this.pos = pos;
         }
 
-        public File withPos(int pos) {
-            return new File(this.id, pos);
+        public FileBlock withPos(int pos) {
+            return new FileBlock(this.id, pos);
         }
 
         public long getFileChecksum() {
@@ -118,7 +189,7 @@ public enum Day9 implements Solver<Long, Integer> {
                 return false;
             if (getClass() != obj.getClass())
                 return false;
-            File other = (File) obj;
+            FileBlock other = (FileBlock) obj;
             if (id != other.id)
                 return false;
             if (pos != other.pos)
