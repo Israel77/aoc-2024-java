dab9f75324d01a0286f573a1355713ba2ffd6cdf
Israel Batista <israel.silvino@gmail.com>
2024-12-13 21:08:29 -0300

    feat: Add methods to ease conversion between Pairs of integral types

--- src/main/java/com/adventofcode/util/Pair.java
+++ src/main/java/com/adventofcode/util/Pair.java
@@ -1,5 +1,7 @@
 package com.adventofcode.util;
 
+import java.math.BigInteger;
+import java.util.Optional;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
@@ -26,6 +28,31 @@ public record Pair<F, S>(F first, S second) {
 
     public static Pair<Integer, Integer> sum(Pair<Integer, Integer> a, Pair<Integer, Integer> b) {
         return new Pair<>(a.first() + b.first(), a.second() + b.second());
+    }
+
+    public static Pair<Long, Long> intToLong(Pair<Integer, Integer> pair) {
+        return new Pair<>(pair.first().longValue(), pair.second().longValue());
+    }
+
+    public static Optional<Pair<Integer, Integer>> longToInt(Pair<Long, Long> pair) {
+        if (pair.first() > Integer.MAX_VALUE || pair.second() > Integer.MAX_VALUE) {
+            return Optional.empty();
+        }
+
+        return Optional.of(new Pair<>(pair.first().intValue(), pair.second().intValue()));
+    }
+
+    public static Pair<BigInteger, BigInteger> longToBigInteger(Pair<Long, Long> pair) {
+        return new Pair<>(BigInteger.valueOf(pair.first()), BigInteger.valueOf(pair.second()));
+    }
+
+    public static Optional<Pair<Long, Long>> bigIntegerToLong(Pair<BigInteger, BigInteger> pair) {
+        if (pair.first().compareTo(BigInteger.valueOf(Long.MAX_VALUE)) > 0
+                || pair.second().compareTo(BigInteger.valueOf(Long.MAX_VALUE)) > 0) {
+            return Optional.empty();
+        }
+
+        return Optional.of(new Pair<>(pair.first().longValue(), pair.second().longValue()));
     }
 
     /**
